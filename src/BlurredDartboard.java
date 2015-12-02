ba7ffeb9e8174db37598ae1b9194bac4db3dffaf
Dan Borthwick <danborthwick@hotmail.com>
2015-12-02 01:57:33 +0000

    BlurredDartboard

--- src/BlurredDartboard.java
+++ src/BlurredDartboard.java
@@ -1,72 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.Arrays;
 
 public class BlurredDartboard {
 
-    static class Throw {
-        private final int score;
-        private final int darts;
-
-        public Throw(int score, int darts) {
-            this.score = score;
-            this.darts = darts;
-        }
-
-        public int getScore() { return score; }
-        public int getDarts() { return darts; }
-
-        public double scorePerDart() { return score / darts; }
-
-    }
-
     public static int minThrows(int[] points, int p) {
-        List<Throw> availableThrows = makeAvailableThrows(points);
-
-        int remainingScore = p;
-        int availableThrowIndex = 0;
-        Stack<Throw> throwStack = new Stack();
-
-        while (remainingScore != 0) {
-            while ((availableThrowIndex < availableThrows.size())
-                    && (availableThrows.get(availableThrowIndex).getScore() > remainingScore)) {
-                availableThrowIndex++;
-            }
-
-            if (availableThrowIndex < availableThrows.size()) {
-                Throw pushed = availableThrows.get(availableThrowIndex);
-                throwStack.push(pushed);
-                remainingScore -= pushed.score;
-            }
-            else {
-                Throw popped = throwStack.pop();
-                availableThrowIndex = availableThrows.indexOf(popped) + 1;
-                remainingScore += popped.score;
-            }
-        }
-
-        return throwStack.stream().mapToInt(Throw::getDarts).sum();
-    }
-
-    private static List<Throw> makeAvailableThrows(int[] points) {
-        List<Throw> availableThrows = new ArrayList();
+        int maxSingleThrow = 0;
+        int unknownPoints = ((points.length + 1) * points.length) / 2;
+        int numUnknownSectors = points.length;
+        Arrays.sort(points);
 
         for (int point : points) {
             if (point > 0) {
-                availableThrows.add(new Throw(point, 1));
+                if (point > maxSingleThrow) {
+                    maxSingleThrow = point;
+                }
+                unknownPoints -= point;
+                numUnknownSectors--;
             }
         }
 
-        int sumOfKnownSectors = availableThrows.stream().mapToInt(Throw::getScore).sum();
-        int sumOfAllSectors = ((points.length + 1) * points.length) / 2;
-        int numUnknownSectors = points.length - availableThrows.size();
+        int dartsThrown = 0;
+        int remainingScore = p;
 
-        if (numUnknownSectors > 0) {
-            int sumOfUnknownSectors = sumOfAllSectors - sumOfKnownSectors;
-            availableThrows.add(new Throw(sumOfUnknownSectors, numUnknownSectors));
+        if ((numUnknownSectors > 0) && ((double)unknownPoints / numUnknownSectors) > maxSingleThrow) {
+            int numSpreads = remainingScore / unknownPoints;
+            remainingScore -= numSpreads * unknownPoints;
+            dartsThrown = numSpreads * numUnknownSectors;
         }
 
-        availableThrows.sort((throw1, throw2) -> Double.compare(throw2.scorePerDart(), throw1.scorePerDart()));
-        return availableThrows;
+        if (remainingScore > 0) {
+            int knownMaxSingleThrows = (int) Math.ceil((double)remainingScore / maxSingleThrow);
+
+            int unknownRequired = remainingScore;
+            int worstCaseUnknownThrows = 0;
+
+            for (int unknownPointCandidate = 1; unknownRequired > 0 && unknownPointCandidate <= points.length; unknownPointCandidate++) {
+                if (isKnown(unknownPointCandidate, points)) {
+                    continue;
+                }
+
+                worstCaseUnknownThrows++;
+                unknownRequired -= unknownPointCandidate;
+            }
+
+            if (worstCaseUnknownThrows == 0)
+                worstCaseUnknownThrows = Integer.MAX_VALUE;
+
+            dartsThrown += Math.min(knownMaxSingleThrows, worstCaseUnknownThrows);
+        }
+
+        return dartsThrown;
+    }
+
+    private static boolean isKnown(int i, int[] points) {
+        for (int point : points) {
+            if (i == point) return true;
+        }
+
+        return false;
     }
 }
