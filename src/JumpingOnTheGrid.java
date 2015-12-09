a2199cfa008ae615b0e22bddca707a2febe5d053
Dan Borthwick <danborthwick@hotmail.com>
2015-12-09 21:33:02 +0000

    JumpingOnTheGrid for small T

--- src/JumpingOnTheGrid.java
+++ src/JumpingOnTheGrid.java
@@ -27,39 +27,65 @@ public class JumpingOnTheGrid {
 
         public void compute() {
             for (int t = 0; t < T; t++) {
-                for (int yFrom = 0; yFrom < grid.length; yFrom++) {
-                    for (int xFrom = 0; xFrom < grid.length; xFrom++) {
-                        long eFrom = maxE[t][yFrom * grid.length + xFrom];
+                for (int y = 0; y < grid.length; y++) {
+                    for (int x = 0; x < grid.length; x++) {
 
-                        if (eFrom > 0) {
-                            eMaxStep(xFrom, yFrom, 0, -1, eFrom, t + 1);
-                            eMaxStep(xFrom, yFrom, 0, 1, eFrom, t + 1);
-                            eMaxStep(xFrom, yFrom, -1, 0, eFrom, t + 1);
-                            eMaxStep(xFrom, yFrom, 1, 0, eFrom, t + 1);
+                        long eFrom = getMaxE(t, x, y);
+                        if (eFrom >= 0) {
+                            int tTo = t + 1;
+
+                            step(tTo, x, y, 0, -1, eFrom);
+                            step(tTo, x, y, 0, 1, eFrom);
+                            step(tTo, x, y, -1, 0, eFrom);
+                            step(tTo, x, y, 1, 0, eFrom);
+
+                            setMaxE(tTo, x, y, eFrom + energyIncreaseAt(x, y));
                         }
                     }
                 }
             }
+        }
 
-
+        private long energyIncreaseAt(int x, int y) {
+            char c = grid[y].charAt(x);
+            if (c >= '0' && c <= '9') {
+                return c - '0';
+            }
+            else if (c >= 'a' && c <= 'z') {
+                return c - 'a' + 10;
+            }
+            else if (c >= 'A' && c <= 'Z') {
+                return c - 'A' + 36;
+            }
+            else {
+                return 0;
+            }
         }
 
         public long result() {
             return maxE[T][indexOf(END)];
         }
 
-        private void eMaxStep(int xFrom, int yFrom, int dx, int dy, long eFrom, int tTo) {
+        private void step(int tTo, int xFrom, int yFrom, int dx, int dy, long eFrom) {
             for (int xTo = xFrom + dx, yTo = yFrom + dy;
                  validCell(xTo, yTo) && canLandOn(xTo, yTo);
                  xTo += dx, yTo += dy) {
+
                 int dist = Math.abs(yTo - yFrom) + Math.abs(xTo - xFrom);
                 long eTo = eFrom - (dist * dist);
-                long eOrig = maxE[tTo][yTo * grid.length + xTo];
 
-                if (eTo > eOrig) {
-                    maxE[tTo][yTo * grid.length + xTo] = eTo;
+                if (eTo > getMaxE(tTo, xTo, yTo)) {
+                    setMaxE(tTo, xTo, yTo, eTo);
                 }
             }
+        }
+
+        private void setMaxE(int t, int x, int y, long e) {
+            maxE[t][y * grid.length + x] = e;
+        }
+
+        private long getMaxE(int t, int x, int y) {
+            return maxE[t][y * grid.length + x];
         }
 
         private boolean validCell(int x, int y) {
